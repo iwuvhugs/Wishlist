80b525edc35273d131f26dc63c7551111934a288
iwuvhugs <ikirill2323@gmail.com>
2016-03-21 13:34:50 -0400

    Model and tests created

---
+++ src/test/java/model/ProductTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author iwuvhugs
+ */
+public class ProductTest {
+
+    public ProductTest() {
+    }
+
+    @org.junit.Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
