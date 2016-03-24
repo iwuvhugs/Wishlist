dd8821b40d5f9b9fad59124510c99167ddd5e92a
c0611751@mylambton.ca <c0611751@mylambton.ca>
2016-03-24 13:45:25 -0400

    filled out theme page and some tests for theme

--- src/main/java/model/Theme.java
+++ src/main/java/model/Theme.java
@@ -7,5 +7,40 @@ package model;
 
 
 public class Theme {
+    private int id_theme;
+    private String title;
+
+    public int getId_theme() {
+        return id_theme;
+    }
+
+    public void setId_theme(int id_theme) {
+        this.id_theme = id_theme;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Theme() {
+       id_theme = -1;
+       title = "default";
+    }
+
+    public Theme(int id_theme, String title) {
+        this.id_theme = id_theme;
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Theme{" + "id_theme=" + id_theme + ", title=" + title + '}';
+    }
+    
+    
     
 }
--- src/test/java/model/ThemeTest.java
+++ src/test/java/model/ThemeTest.java
@@ -5,8 +5,12 @@
  */
 package model;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -17,10 +21,98 @@ public class ThemeTest {
     public ThemeTest() {
     }
 
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    @BeforeClass
+    public static void setUpClass() throws Exception {
     }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetId_user() {
+        System.out.println("getId_user");
+        User instance = new User();
+        int expResult = -1;
+        int result = instance.getId_user();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getId_theme method, of class Theme.
+     */
+    @Test
+    public void testGetId_theme() {
+        System.out.println("getId_theme");
+        Theme instance = new Theme();
+        int expResult = 0;
+        int result = instance.getId_theme();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("could not get theme_id");
+    }
+
+    /**
+     * Test of setId_theme method, of class Theme.
+     */
+    @Test
+    public void testSetId_theme() {
+        System.out.println("setId_theme");
+        int id_theme = 0;
+        Theme instance = new Theme();
+        instance.setId_theme(id_theme);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("could not set id.");
+    }
+
+    /**
+     * Test of getTitle method, of class Theme.
+     */
+    @Test
+    public void testGetTitle() {
+        System.out.println("getTitle");
+        Theme instance = new Theme();
+        String expResult = "default";
+        String result = instance.getTitle();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("could not get title");
+    }
+
+    /**
+     * Test of setTitle method, of class Theme.
+     */
+    @Test
+    public void testSetTitle() {
+        System.out.println("setTitle");
+        String title = "";
+        Theme instance = new Theme();
+        instance.setTitle(title);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("could not set title");
+    }
+
+    /**
+     * Test of toString method, of class Theme.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Theme instance = new Theme();
+        String expResult = "Theme{" + "id_theme=-1, title=default}";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("toString fail to work");
+    }
+    
 
 }
