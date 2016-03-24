c618b5b0a777db4a6c9d6e4cf9a24d5f00e94537
iwuvhugs <ikirill2323@gmail.com>
2016-03-24 12:40:06 -0400

    Wishlist model class

--- src/main/java/model/Wishlist.java
+++ src/main/java/model/Wishlist.java
@@ -5,7 +5,53 @@
  */
 package model;
 
+import java.util.ArrayList;
 
 public class Wishlist {
-    
+
+    private int id_wishlist;
+    private User user;
+    private Theme theme;
+
+    private ArrayList<Product> products;
+
+    public Wishlist() {
+        id_wishlist = -1;
+        user = new User();
+        theme = new Theme();
+        products = new ArrayList<>();
+    }
+
+    public int getId_wishlist() {
+        return id_wishlist;
+    }
+
+    public void setId_wishlist(int id_wishlist) {
+        this.id_wishlist = id_wishlist;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public void setTheme(Theme theme) {
+        this.theme = theme;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
 }
