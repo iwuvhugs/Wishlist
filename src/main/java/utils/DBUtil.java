c4d06a6958d0dc903e7c4fae5b67b9e5015878bb
iwuvhugs <ikirill2323@gmail.com>
2016-04-13 14:52:59 -0400

    Added OpenShift connection credentials

--- src/main/java/utils/DBUtil.java
+++ src/main/java/utils/DBUtil.java
@@ -18,24 +18,43 @@ import java.util.logging.Logger;
 public class DBUtil {
 
     /**
-     * 
-     * @return
-     * @throws SQLException 
+     *
+     * @return @throws SQLException
      */
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() {
+        Connection connection;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
+                return null;
+            }
+            String host = "localhost";
+            String port = "3306";
+            String db = "wishlist";
+            String user = "root";
+            String pass = "";
+            String jdbc = "jdbc:mysql://" + host + ":" + port + "/" + db;
+
+            connection = DriverManager.getConnection(jdbc, user, pass);
+        } catch (Exception e) {
+            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, e);
+            try {
+                String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+                String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+                String name = "sample";
+                String url = "jdbc:mysql://" + host + ":" + port + "/" + name;
+                String username = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+                String password = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException ex) {
+                Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
+                connection = null;
+            }
         }
-        String host = "localhost";
-        String port = "3306";
-        String db = "wishlist";
-        String user = "root";
-        String pass = "";
-        String jdbc = "jdbc:mysql://" + host + ":" + port + "/" + db;
-        return DriverManager.getConnection(jdbc, user, pass);
+        return connection;
     }
 
 }
